f4ceb9c3204a18bf839e01b1643c860b7ffce88b
angauber <angauber@student.le-101.fr>
2019-04-23 17:45:51 +0200

    iz

--- app.js
+++ app.js
@@ -13,17 +13,27 @@ app.use('/srt', express.static('data/subs'));
 app.get('/', function(req, res) {
 	res.render('home.ejs');
 })
+.get('/42auth', function(req, res) {
+	if (req.query.code !== undefined) {
+		console.log('making reqest');
+		console.log(request.post('https://api.intra.42.fr/oauth/token').form({grand_type: 'client_credentials', client_id: '31d8a3dc762efc192b63a8877cd71ff77a004d348da9e2d92497c806e272c374', client_secret: '8fc87ed6fb9bfd4f623496efdaad63fb87bc473486c24e9a5d03d2a138de1c54', code: req.query.code}));
+		console.log('request post');
+	}
+	else {
+		res.status(404).end();
+	}
+})
 .get('/query', function(req, res) {
 	if (req.query.name !== undefined) {
-		request('https://yts.am/api/v2/list_movies.json?sort_by=download_count&query_term=' + req.query.name + '&limit=48', function (error, response, body) {
+		request('https://v2.sg.media-imdb.com/suggests/' + req.query.name.charAt(0) + '/' + req.query.name + '.json', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-				const info = JSON.parse(body)
-				if (info.data.movies) {
-					res.render('search.ejs', {'data' : info.data.movies})
-				}
-				else {
-					res.render('search.ejs', {'data' : false});
-				}
+				var jsonpData = body;
+				var startPos = jsonpData.indexOf('({');
+				var endPos = jsonpData.indexOf('})');
+				var jsonString = jsonpData.substring(startPos+1, endPos+1);
+				var json = JSON.parse(jsonString);
+				console.log(json.d);
+				// res.render('search.ejs', {'data' : info.data.movies})
 			}
 			else {
 				res.status(404).end();
