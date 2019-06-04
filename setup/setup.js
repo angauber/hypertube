4322ab67122c31aca79b6606c0df4fe710f510be
Antoine Gaubert <angauber@z4r1p4.le-101.fr>
2019-06-04 13:40:57 +0200

    wipe

--- setup/setup.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/hypertube', { useNewUrlParser: true, keepAlive: true, keepAliveInitialDelay: 10000 });
-
-let db = mongoose.connection;
-
-module.exports = db;
